62ae3eeb375dcbcc44c676b7be81baf59541a4ec
chengzhuo <chengzhuo@xiaomi.com>
2020-11-16 18:30:17 +0800

    兼容react-native

--- runtime/resolve-value.js
+++ runtime/resolve-value.js
@@ -1,4 +1,7 @@
 function resolveValue(args) {
+  if (args[0] && args[0].nativeEvent && args[0].nativeEvent.text) {
+    return args[0].nativeEvent.text
+  }
   var target = args[0] && args[0].target;
   if(target && target.nodeType === 1) {
     return target.value;
